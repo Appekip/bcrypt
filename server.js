dab1f45ea89201a139ed1efae214c9b04eedd05f
Appekip <arttu.peramaki@gmail.com>
2023-11-09 15:52:13 +0200

    gamin

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
+const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res);
+    })
+});
+
 
 //END_ASYNC
 
